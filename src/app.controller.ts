cc956886518f44e11ad52d9742e1166df62c8f56
¡Duque <atanaelleonardo@gmail.com>
2022-12-09 04:12:12 -0300

    𝑼𝒔𝒊𝒏𝒈 𝒑𝒓𝒊𝒔𝒎𝒂 𝒔𝒆𝒓𝒗𝒊𝒄𝒆

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from "@nestjs/common";
-import { AppService } from "./app.service";
+import { PrismaService } from "./database/prisma.service";
 
-@Controller()
+@Controller("app")
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private prisma: PrismaService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("hello")
+  async getHello() {
+    const member = await this.prisma.rocketTeamMember.create({
+      data: {
+        id: "Capitao",
+        name: "Guilherme Capitao",
+        function: "Alcançar coisas no alto",
+      },
+    });
+    return { member };
   }
 }
